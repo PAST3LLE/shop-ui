1d9ba49c54a4a42150074b090b456bd9ca5e42ec
daveo <21335563+W3stside@users.noreply.github.com>
2025-11-15 14:02:38 +0000

    update: new drop (#80)
    updates
    
    updates: pt.2

--- src/components/PagesComponents/components/SaleBanner.tsx
+++ src/components/PagesComponents/components/SaleBanner.tsx
@@ -1,8 +1,10 @@
 import { Row, RowProps } from '@past3lle/components'
+import { useIsExtraSmallMediaWidth } from '@past3lle/hooks'
 import { PNG_LogoCircle_2x } from '@past3lle/assets'
 import { Text } from '@/components/Text'
 
 export function SaleBanner(props: RowProps) {
+  const isMobileWidth = useIsExtraSmallMediaWidth()
   return (
     <Row
       justifyContent="center"
@@ -14,8 +16,14 @@ export function SaleBanner(props: RowProps) {
       gap="0.5rem"
       {...props}
     >
-      <img src={PNG_LogoCircle_2x as unknown as string} />
-      <Text.ProductText fontSize="2.5rem">{process.env.NEXT_PUBLIC_SALE}</Text.ProductText>
+      <img
+        src={PNG_LogoCircle_2x as unknown as string}
+        style={{ marginLeft: 'auto', height: isMobileWidth ? '80%' : '100%' }}
+      />
+      <Text.ProductText fontSize={isMobileWidth ? '1.75rem' : '2.25rem'} marginLeft="1rem" fontWeight={700}>
+        {isMobileWidth ? process.env.NEXT_PUBLIC_SALE_MOBILE : process.env.NEXT_PUBLIC_SALE}
+      </Text.ProductText>
+      <div style={{ margin: '0 1rem 0 auto', cursor: 'pointer', fontWeight: 800, fontSize: '1.5rem' }}>X</div>
     </Row>
   )
 }
